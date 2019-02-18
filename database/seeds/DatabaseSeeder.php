d753888de4bad8dcf38473773a8190e28d925077
Gatozaurio <tavoraromero@gmail.com>
2019-02-18 15:35:43 +0100

    Añadida tabla puvot concert_user al seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,10 +13,17 @@ class DatabaseSeeder extends Seeder
     {
         $concerts = Factory(App\Concert::class, 25)->create();
 		$artists = Factory(App\Artist::class, 15)->create();
+		$users = Factory(App\User::class, 50)->create();
 
 		$concerts->each(function(App\Concert $concert) use ($artists){
 			$concert->artists()->attach(
 				$artists->random(random_int(1,15))
+			);
+		});
+
+		$concerts->each(function(App\Concert $concert) use ($users){
+			$concert->users()->attach(
+				$users->random(random_int(1,25))
 			);
 		});
     }
